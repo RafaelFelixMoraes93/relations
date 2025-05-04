23baf0ce9feeea779bb91a748c021098dae909a2
Rafael <rafael.felix.moraes@gmail.com>
2025-05-04 15:19:59 -0300

    Feat novo arquivo de rota api.php

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Faker\Provider\ar_EG\Address;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AddressController;
+
+    Route::get("/users", [UserController::class, 'index']);
+
+    Route::get('/addresses', [AddressController::class,'index']);
