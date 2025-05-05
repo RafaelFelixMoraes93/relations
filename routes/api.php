0ec8194e2bbb8ed4565a88a1291609cf11735f11
Rafael <rafael.felix.moraes@gmail.com>
2025-05-05 19:47:08 -0300

    Refactor adicionado rotas de post

--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,12 @@ use App\Http\Controllers\AddressController;
 
     Route::get("/users", [UserController::class, 'index']);
 
+    Route::get('/users/{id}', [UserController::class, 'findOne']);
+
+    Route::post('/user', [UserController::class,'postUser']);
+
     Route::get('/addresses', [AddressController::class,'index']);
+
+    Route::get('/addresses/{id}', [AddressController::class,'findOne']);
+
+    Route::post('/address', [AddressController::class,'postAddress']);
