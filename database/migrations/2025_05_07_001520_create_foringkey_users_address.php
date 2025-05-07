ee784b0534556271905daadc2d8145cb9b4c7492
Rafael <rafael.felix.moraes@gmail.com>
2025-05-06 21:40:41 -0300

    Refactor trocada on table de users para addreses

--- database/migrations/2025_05_07_001520_create_foringkey_users_address.php
+++ database/migrations/2025_05_07_001520_create_foringkey_users_address.php
@@ -15,7 +15,7 @@ return new class extends Migration
             //
             $table->foreign('address_id')
             ->references('id')
-            ->on('users')
+            ->on('addresses')
             ->onDelete('SET NULL');
         });
     }
