ad6a48a4cd403925d61461b3bf127e61b985cf2c
Rafael <rafael.felix.moraes@gmail.com>
2025-05-06 21:41:51 -0300

    Refactor adicionado down

--- database/migrations/2025_05_07_001520_create_foringkey_users_address.php
+++ database/migrations/2025_05_07_001520_create_foringkey_users_address.php
@@ -27,6 +27,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             //
+            $table->dropForeign(['address_id']);
         });
     }
 };
